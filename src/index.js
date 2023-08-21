0970c1c24f466d524ea1996eee26aedfd2c2170a
Justin Rhodes <justinr@tri.be>
2023-08-21 10:31:59 -0500

    rename import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Router from "./components/Router";
+import Router from "./Components/Router";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root")); // createRoot(container!) if you use TypeScript
